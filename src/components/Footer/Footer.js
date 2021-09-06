c4f66814d7ad9dce87e80e67ae9c1354cf19ad08
uai <noema01@outlook.jp>
2021-09-06 18:50:42 +0900

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>©Yuma Matsuzaki, uai </p>
+      <a href="https://github.com/Sheerlore">My github →</a>
+    </footer>
+  );
+}
+
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
